5108efb7a0a2c3179fa6de455ed0737917daba94
ChilliPhantom <129586206+ChilliPhantom@users.noreply.github.com>
2025-05-11 20:09:29 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: resolve(__dirname, 'index.html') // <- absolute path
+      input: resolve(__dirname, 'index.html')
     }
   }
-})
+});
