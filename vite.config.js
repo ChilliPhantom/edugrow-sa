3edce97b0ddbdc9f1f0c3d265299563bdff442f4
ChilliPhantom <129586206+ChilliPhantom@users.noreply.github.com>
2025-05-11 21:59:47 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import react from '@vitejs/plugin-react';
 
 export default {
-  plugins: [react()],
+  plugins: [
+    react({
+      jsx: 'react', // Explicitly enable JSX transform (for React 17 and higher)
+    }),
+  ],
 };
