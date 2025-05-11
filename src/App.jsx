8d6f1cc65baf9de90a6f7d3b8c19a5e6cba14d82
ChilliPhantom <129586206+ChilliPhantom@users.noreply.github.com>
2025-05-11 21:39:48 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import LandingPage from './pages/LandingPage';
-import RegisterPage from './pages/RegisterPage';
-import LoginPage from './pages/LoginPage';
-import Dashboard from './pages/Dashboard';
-import AdminPanel from './pages/AdminPanel';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import Dashboard from "./pages/Dashboard";
+import AdminPanel from "./pages/AdminPanel";
 
 function App() {
   return (
