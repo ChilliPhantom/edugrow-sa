2e6a85a4647e01e6b80635af8c05889f496a1ff6
ChilliPhantom <129586206+ChilliPhantom@users.noreply.github.com>
2025-05-11 22:04:34 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
 import react from '@vitejs/plugin-react';
 
 export default {
-  plugins: [
-    react({
-      jsx: 'react', // Explicitly enable JSX transform (for React 17 and higher)
-    }),
-  ],
+  plugins: [react()],
 };
